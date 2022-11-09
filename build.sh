dc1a8852e3368a7c49ca9a3134f6d0bc1b0e12c1
Linn Bedroc <blindcrone@tuta.io>
2022-11-09 06:43:37 -0800

    Added lto to build

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-g++ -O3 -Wall -shared -std=c++20 -fPIC -I/usr/include/python3.10 -I/usr/lib/python3.10/site-packages/pybind11/include src/pisces.cc -o pisces.cpython-310-x86_64-linux-gnu.so
+#!/bin/bash
+g++ -O3 -Wall -shared -std=c++20 -flto=auto -fPIC -I/usr/include/python3.10 -I/usr/lib/python3.10/site-packages/pybind11/include src/pisces.cc -o pisces.cpython-310-x86_64-linux-gnu.so
